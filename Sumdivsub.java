4409b658915a5f164eb480bc7d09393dabad58ea
Sharminfatema <107508084+Sharminfatema@users.noreply.github.com>
2022-06-20 22:30:12 +0600

    Add files via upload

---
+++ Sumdivsub.java
@@ -0,0 +1,27 @@
+package lecture1;
+import java.util.Scanner;
+public class Sumdivsub {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a,b,result;
+		 a=10;
+		 b=10;
+		 
+		 result=a+b;
+				 System.out.println(result);
+				 
+				 result=a-b;
+				 System.out.println(result);
+				 result=a*b;
+				 System.out.println(result);
+				 result=a/b;
+				 System.out.println(result);
+				 
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
