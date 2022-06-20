2f10e1ec14fdecae9b0a403348acca8541282a73
Sharminfatema <107508084+Sharminfatema@users.noreply.github.com>
2022-06-20 21:58:01 +0600

    Add files via upload

---
+++ input output.java
@@ -0,0 +1,18 @@
+
+package array1;
+import java.util.Scanner;
+
+public class Array {
+	//sum of two no.
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a=10, b=5;
+		
+		System.out.println(a+b);
+		//System.out.println("sharmin");
+		// TODO Auto-generated method stub
+
+	}
+
+}
